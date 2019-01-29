5fd8eba29cb26eadf77d74785e999a7a1c5bf9d8
Julia Roggatz <jroggatz@kayak.com>
2019-01-28 22:45:45 -0500

    Initial Commit

---
+++ ScreenEdgePencilAngleBug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScreenEdgePencilAngleBug
+//
+//  Created by Julia Roggatz on 28.01.19.
+//  Copyright © 2019 Ink & Switch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
